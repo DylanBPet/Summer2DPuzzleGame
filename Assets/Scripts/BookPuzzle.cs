12e9925a60b2d715d8bc7a0f34b1605195cb461d
DylanBPet <dylanpetroff13@gmail.com>
2026-07-15 10:35:51 -0400

    Worked more on book puzzle

--- Assets/Scripts/BookPuzzle.cs
+++ Assets/Scripts/BookPuzzle.cs
@@ -11,6 +11,9 @@ public class BookPuzzle : MonoBehaviour
     private int? pickedUpBookNumber = null;
     private Vector3 originalPos = new Vector3(0f, 0f, 0f);
 
+    ///////// ROW AND SECTION VARIABLES
+    public List<GameObject> rowSections;
+
     void Start()
     {
         pickedUpBookNumber = null;
@@ -36,6 +39,7 @@ public class BookPuzzle : MonoBehaviour
             //doubley make sure to set BOTH to NULL during the PuttingDownBook callbackcontext so it is reset for the next book
         }
 
+        
 
     }
     public void PickingUpBooks(InputAction.CallbackContext context)
@@ -73,11 +77,39 @@ public class BookPuzzle : MonoBehaviour
             Debug.Log("Book " + pickedUpBookNumber + " Has Been Placed");
 
             int bookNumber = pickedUpBookNumber.Value;
-            books[bookNumber].transform.position = originalPos;
-            
+
+            //find the closest rowSection to the book//
+
+            //set the indext to out of range so there is out of range errors
+            int closestIndex = -1;
+            //set the closest distance to THE MAX so there is no errors
+            float closestDist = float.MaxValue;
+
+            for (int i = 0; i < rowSections.Count; i++)
+            {
+                float dist = Vector2.Distance(books[bookNumber].transform.position, rowSections[i].transform.position);
+                //if the new distance is closer than the previous ones, replace the index and closest dist
+                if (dist < closestDist)
+                {
+                    closestDist = dist;
+                    closestIndex = i;
+                }
+            }
+            //if the closest indext is not -1 (error) and is less than 2, move the book to the closest rowSection
+            if (closestIndex != -1 && closestDist < 2)
+            {
+                books[bookNumber].transform.position = rowSections[closestIndex].transform.position;
+            }
+            //if it is out of range, move it back to its original position
+            else
+            {
+                //if dist is more than a number, put it back at original position
+                books[bookNumber].transform.position = originalPos;
+            }
             //reset the book number that is being picked up back to unasigned
             pickedUpBookNumber = null;
         }
+           
     }
     public void BookPickedUp(int? i)
     {
