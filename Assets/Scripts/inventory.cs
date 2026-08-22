808038b18dab039771a91cc9df03d53cc7667de4
DylanBPet <dylanpetroff13@gmail.com>
2026-08-22 19:32:16 -0400

    fixed compile error

--- Assets/Scripts/inventory.cs
+++ Assets/Scripts/inventory.cs
@@ -1,10 +1,8 @@
 
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using static UnityEditor.Progress;
 
 public class Invintory : MonoBehaviour
 {
