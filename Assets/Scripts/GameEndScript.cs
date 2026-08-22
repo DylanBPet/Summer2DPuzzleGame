f26e4a49edccc6dd9c95e41a19d77ed0c223e747
DylanBPet <dylanpetroff13@gmail.com>
2026-08-22 19:21:24 -0400

    DEMO READY YAAYAY

--- Assets/Scripts/GameEndScript.cs
+++ Assets/Scripts/GameEndScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class GameEndScript : MonoBehaviour
 {
+    public GameObject gameEnd;
+
     public TextMeshProUGUI paragraphOne;
     public TextMeshProUGUI paragraphTwo;
     public TextMeshProUGUI paragraphThree;
@@ -11,8 +13,17 @@ public class GameEndScript : MonoBehaviour
     public GameObject buttonOne;
     public GameObject buttonTwo;
     public GameObject buttonThree;
+    public GameObject buttonFour;
 
     public AudioManager audioScript;
+
+
+    public GameObject newsPaper;
+    public GameObject newsPaperEndDest;
+    public GameObject newsPaperEndRotation;
+    public GameObject newsPaperStartPos;
+
+    public GameObject endCreditScreen;
 
     public void StartFinalCoroutine()
     {
@@ -76,8 +87,44 @@ public class GameEndScript : MonoBehaviour
 
     public void ThrownNewspaper()
     {
+        buttonThree.SetActive(false);
         audioScript.PlaySoundEffect(audioScript.keyboardClicking);
         buttonThree.SetActive(false);
+
        //the newspaper gets thrown onto the screen above the words of paragraph 3
+       newsPaper.SetActive(true);
+        StartCoroutine(ThrowNewsPaper());
+        StartCoroutine(RotateNewsPaper());
+
+    }
+
+    IEnumerator ThrowNewsPaper()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * 0.8f;
+            newsPaper.transform.position = Vector2.Lerp(newsPaperStartPos.transform.position, newsPaperEndDest.transform.position, t);
+            yield return null;
+        }
+        
+    }
+
+    IEnumerator RotateNewsPaper()
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime * 0.5f;
+            newsPaper.transform.rotation = Quaternion.Lerp(newsPaperStartPos.transform.rotation, newsPaperEndRotation.transform.rotation, t);
+            yield return null;
+        }
+        buttonFour.SetActive(true);
+    }
+
+    public void ShowCredits()
+    {
+        endCreditScreen.SetActive(true);
+        gameEnd.SetActive(false);
     }
 }
