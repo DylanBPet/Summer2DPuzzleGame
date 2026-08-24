29bd86579566c040d15bb64cb43bf8b1253d6503
DylanBPet <dylanpetroff13@gmail.com>
2026-08-24 18:09:34 -0400

    Changed some audio settings, attempted to get rid of buffer, yet could not. hopefully its just a unity issue and fixes when a build is made

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -19,14 +19,18 @@ public class GameStart : MonoBehaviour
         audioScript.PlayBackgroundNoise(audioScript.whiteNoise);
         yield return new WaitForSeconds(1f);
         audioScript.PlaySoundEffect(audioScript.keyboardClicking);
+        yield return new WaitForSeconds(0.2f);
         titleCard.SetActive(true);
         yield return new WaitForSeconds(3f);
         audioScript.PlaySoundEffect(audioScript.keyboardClicking);
+        yield return new WaitForSeconds(0.2f);
         textCanvas.SetActive(true);
         yield return new WaitForSeconds(4f);
         audioScript.PlaySoundEffect(audioScript.keyboardClicking);
         audioScript.StopBackgroundMusic();
+        yield return new WaitForSeconds(0.1f);
         audioScript.PlayBackgroundNoise(audioScript.backgroundMusic);
+        yield return new WaitForSeconds(0.1f);
         mainScene.SetActive(true);
         gameStartScreen.SetActive(false);
         yield return null;
